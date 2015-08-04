3528db89cc436f72319964e1a37ca77e71d2f0a0
Kuntau <kuntau17@gmail.com>
2015-08-04 18:06:24 +0800

    $http preparation

--- public/js/app.js
+++ public/js/app.js
@@ -4,13 +4,21 @@
 * Description
 */
 
-angular.module('papweb', ['ui.router'])
+angular.module('papweb', [ 'ui.router' ])
 
-.controller('HeaderCtrl', ['$scope', function($scope){
-  var vm = this;
+  .controller('HeaderCtrl', ['$scope', function($scope){
+    var vm = this;
 
-  angular.element(document).ready(function () {
-    angular.element('.bxslider').bxSlider();
-  });
+    angular.element(document).ready(function () {
+      angular.element('.bxslider').bxSlider();
+    });
+  }])
 
-}]);
+  .controller('RouteCtrl', [ '$scope', '$http', function ($scope, $http) {
+    var vm = this;
+
+    $http.get('http://papsb.com.my/data/routes.json')
+      .success(function (data) {
+        vm.routes = data;
+      })
+  }])
