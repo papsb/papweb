6d3ec44accdfe1b53e98e676dbb93aafe2ce59cc
Kuntau <kuntau17@gmail.com>
2016-09-28 12:02:00 +0800

    regress, after all we still need the listen function....

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,21 @@ var gulp = require('gulp');
 var serve = require('gulp-serve');
 var livereload = require('gulp-livereload');
 
+livereload.listen({
+  "server": "localhost",
+  "port": "35729",
+  "reloadPage": "index.html"
+});
+
 gulp.task('serve', serve({
   root: [ 'public' ],
-  hostname: '0.0.0.0', //fix for v1.4 so we can access from other PC
+  hostname: '0.0.0.0', // this fix gulp-serve v1.4 to open from another PC
   port: 8000
 }));
 
 gulp.task('watch', ['serve'], function () {
   gulp.watch(['!public/libs/**', 'public/**']).on('change', function (file) {
+    livereload.listen();
     livereload.changed(file.path);
   })
 })
