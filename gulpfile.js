02b2789763cb186d1e9d846afdd1b2b62eacca93
Kuntau <kuntau17@gmail.com>
2016-09-28 11:49:09 +0800

    fix for gulp-serve 1.4

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,11 @@ var gulp = require('gulp');
 var serve = require('gulp-serve');
 var livereload = require('gulp-livereload');
 
-
-gulp.task('serve', serve('public'));
+gulp.task('serve', serve({
+  root: [ 'public' ],
+  hostname: '0.0.0.0', //fix for v1.4 so we can access from other PC
+  port: 8000
+}));
 
 gulp.task('watch', ['serve'], function () {
   gulp.watch(['!public/libs/**', 'public/**']).on('change', function (file) {
